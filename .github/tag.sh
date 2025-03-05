59a8024524ac47790151a8da816aeb60f78e98f4
Rassul Yunussov <yunussov@gmail.com>
2025-03-05 19:16:53 +0200

    universal increment of patch

--- .github/tag.sh
+++ .github/tag.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-next_version=v0.0.$(git tag --sort=-v:refname | head -n 1 | sed 's|.*\.||' | awk '{print $1 + 1}')
+prefix_version=$(git tag --sort=-v:refname | head -n 1 | sed 's/[^.]*$//')
+next_version=$prefix_version$(git tag --sort=-v:refname | head -n 1 | sed 's|.*\.||' | awk '{print $1 + 1}')
 git tag $next_version
 git push origin tag $next_version
 gh release create $next_version
