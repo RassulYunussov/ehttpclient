07fc6ae0f148ff5b5f6439480959e13a756935a5
Rassul Yunussov <yunussov@gmail.com>
2025-03-05 18:46:41 +0200

    git tag

--- .github/tag.sh
+++ .github/tag.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 next_version=v0.0.$(git tag --sort=-v:refname | head -n 1 | sed 's|.*\.||' | awk '{print $1 + 1}')
-echo $next_version
-#echo $next_version
-#git tag $next_version
+git tag $next_version
 #gh release create $next_version
 #go list -m github.com/RassulYunussov/ehttpclient@$next_version
