193bf70b71ec70aaa1d97ccc1e25780c1e84f049
Rassul Yunussov <yunussov@gmail.com>
2025-03-05 18:33:47 +0200

    add go list

--- .github/tag.sh
+++ .github/tag.sh
@@ -2,3 +2,4 @@
 next_version=v0.0."$(git tag --sort=-v:refname | head -n 1 | sed 's|.*\.||' | awk '{print $1 + 1}')"
 git tag $next_version
 gh release create $next_version
+go list -m github.com/RassulYunussov/ehttpclient@$next_version
