958f0b874c4d707b5d6e713b4419ba53468f1cf4
Rassul Yunussov <yunussov@gmail.com>
2025-03-05 18:54:08 +0200

    push tag

--- .github/tag.sh
+++ .github/tag.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 next_version=v0.0.$(git tag --sort=-v:refname | head -n 1 | sed 's|.*\.||' | awk '{print $1 + 1}')
 git tag $next_version
+git push origin tag $next_version
 gh release create $next_version
 go list -m github.com/RassulYunussov/ehttpclient@$next_version
